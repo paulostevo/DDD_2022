87c19b18768efd88842e473d57790fbd17ab4054
paulostevo <41132104+paulostevo@users.noreply.github.com>
2022-09-08 21:10:19 -0300

    Created Interfaces and repository generics

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,42 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Message> Message { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DDD_2022;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+    }
+}
